7434a990bbb423b967d03ef381943e7069481bd3
Wouter Swierstra <w.s.swierstra@uu.nl>
2007-04-10 19:52:08 +0000

    Added top level .hs file.

---
+++ src/Test/IOSpec.hs
@@ -0,0 +1,10 @@
+module Test.IOSpec
+  (
+    module Test.IOSpec.IORef
+  , module Test.IOSpec.Concurrent
+  , module Test.IOSpec.Teletype
+  ) where
+
+import Test.IOSpec.Concurrent
+import Test.IOSpec.IORef
+import Test.IOSpec.Teletype
